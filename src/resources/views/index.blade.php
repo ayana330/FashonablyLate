6b1f03e563669dac7e39dd5493baf0b25d25b4d0
ayana <wencaishanben@gmail.com>
2025-04-18 16:12:05 +0900

    Register

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,39 +7,61 @@
     <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
 </head>
 <body>
-    <header class="header">
-        <div class="hedder__inner">
-            <a class="header__logo" href="/">
-                FashonablyLate
-            </a>
-        </div>
-    </header>
+  <header class="header">
+    <div class="hedder__inner">
+        <a class="header__logo" href="/">
+        FashonablyLate</a>
+    </div>
+  </header>
 
-    <main>
-        <div class="contact-form__content">
-          <div class="contact-form__heading">
-            <h2>Register</h2>
+  <main>
+    <div class="contact-form__content">
+        <div class="contact-form__heading">
+        <h2>Register</h2>
         </div>
-        <form class="form">
-            <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">お名前</span>
-            </div>
-            <div class="form__group-content">
-              <div class="form__input--text">
-              <input type="text" name="name" placeholder="例：山田太郎">
-            </div>
-            <div class="form__error">
-                <!--バリデーション機能を実装したら記述します。 -->
-            </div>
+    </div>
+    <form class="form">
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">お名前</span>
+          </div>
+        <div class="form__group-content">
+          <div class="form__input--text">
+            <input type="text" name="name" placeholder="例：山田太郎">
+          </div>
+        <div class="form__error">
+            <!--バリデーション機能を実装したら記述します。 -->
+        </div>
+        </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">メールアドレス</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="email" name="email" placeholder="例：test@example.com" />
+          </div>
+          <div class="form__error">
+            <!--バリデーション機能を実装したら記述します。-->
           </div>
         </div>
-        div class
-            </div>
-          </div>
-        </div>
-            <div class=></div>
-        </form>
+    </div>
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">パスワード</span>
+    </div>
+    <div class="form__group-content">
+      <div class="form__input--text">
+        <input type="password" name="password" placeholder="例：coachtech1106" />
+    </div>
+    <div class="form__error">
+      <!--バリデーション機能を実装したら記述します。-->
+    </div>
+    <div class="form__button">
+      <button class="form__button-submit" type="submit">登録</button>
+    </div>
+      </form>
     </main>
 </body>
 </html>
