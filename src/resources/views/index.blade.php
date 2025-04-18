00f2ac76cad4dff1b5d67a00671aa377234e25e7
ayana <wencaishanben@gmail.com>
2025-04-18 15:02:35 +0900

    index.blade.php

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FashonablyLate</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+</head>
+<body>
+    <header class="header">
+        <div class="hedder__inner">
+            <a class="header__logo" href="/">
+                FashonablyLate
+            </a>
+        </div>
+    </header>
+
+    <main>
+        <div class="contact-form__content">
+          <div class="contact-form__heading">
+            <h2>Register</h2>
+        </div>
+        <form class="form">
+            <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">お名前</span>
+            </div>
+            <div class="form__group-content">
+              <div class="form__input--text">
+              <input type="text" name="name" placeholder="例：山田太郎">
+            </div>
+            <div class="form__error">
+                <!--バリデーション機能を実装したら記述します。 -->
+            </div>
+          </div>
+        </div>
+        div class
+            </div>
+          </div>
+        </div>
+            <div class=></div>
+        </form>
+    </main>
+</body>
+</html>
