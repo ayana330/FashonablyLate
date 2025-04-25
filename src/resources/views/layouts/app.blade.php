32d1ce0b6558f5a2d6d9c845e97503840f59d6a8
ayana <wencaishanben@gmail.com>
2025-04-25 14:46:00 +0900

    20250425

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>お問い合わせフォーム</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}"> <!-- CSS -->
+    @yield('css')
+
 </head>
 <body>
 
