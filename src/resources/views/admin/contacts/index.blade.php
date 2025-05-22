5e26f577578e1ebbb7efda85775770df349c2406
ayana <wencaishanben@gmail.com>
2025-05-22 15:58:57 +0900

    0522

--- src/resources/views/admin/contacts/index.blade.php
+++ src/resources/views/admin/contacts/index.blade.php
@@ -44,5 +44,21 @@
         <button type="submit" class="search-form__button">エクスポート</button>   
     </div>
 
+    <table>
+        <tr>
+            <th>お名前</th>
+            <th>性別</th>
+            <th>メールアドレス</th>
+            <th>お問い合わせの種類</th>
+            <th></th>
+        </tr>
+        <tr>
+            <td>山田 太郎</td>
+            <td>男性</td>
+            <td>test@example.com</td>
+            <td>商品の交換について</td>
+            <td>詳細</td>
+        </tr>
+    </table>
 </form>
 @endsection
